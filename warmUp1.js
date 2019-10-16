3672f050e258672871f70c10fbb7bcece671e013
blusk1n <tahaznazen@gmail.com>
2019-10-16 09:53:22 +0100

    solution added

--- warmUp1.js
+++ warmUp1.js
@@ -1,4 +1,8 @@
 // 1-Create a third variable called z, assign x + y to it, and display it.
 var x = 5;
 var y = 10;
+var z = x + y;
 // 2-calculate the area of a square using varaiables.
+var xArea = x * x;
+var yArea = y * y;
+var zArea = z * z;
