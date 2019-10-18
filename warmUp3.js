2b9fbc47f19c8a94b9edc34f658d8ee222bafb23
blusk1n <tahaznazen@gmail.com>
2019-10-18 09:45:16 +0100

    warmUp 3 solved

--- warmUp3.js
+++ warmUp3.js
@@ -6,3 +6,30 @@
 // 2-write a function that takes an array as an argument and returns the element that is located in the middle of that array.
 
 // 3-Change all the numbers in the array to be multiplied by two for even indexes.
+
+var arrayOfMales = ["taha","ahmed","hamza","amin","firas","adnen"] ;
+var arrayOfFemales = ["ons","haniya","fouz"] ;
+var arrayOfInstructors = ["youssef" , "seif" ,"matt","omar","raghda"] ;
+
+
+function middleOne (arr){
+	var element = "";
+	for( var i = 0 ; i < arr.length ; i++ ){
+		if( arr.length % 2 === 0 ){
+			element =  arr[arr.length/2]
+		}
+		else{
+			element = arr[Math.ceil(arr.length/2)-1]
+		}
+	}
+	return element
+}
+
+function multiByTwo (arr){
+		for( var i = 0 ; i < arr.length ; i++ ){
+			if( i % 2 === 0 ){
+				arr[i] *= 2
+			}
+		}
+		return arr
+}
