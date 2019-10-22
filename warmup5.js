007394f57f4d630b477b654015e397f77de67158
blusk1n <tahaznazen@gmail.com>
2019-10-22 09:48:14 +0100

    warmup 5 added by Taha Znazen

--- warmup5.js
+++ warmup5.js
@@ -7,3 +7,52 @@
 //     -calculate the number of male friends that your class have by writing a function called nbOfMale.
 //     -Write a function searchMates that, given a query and an array of Mates,
 //           searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
+
+
+function creatClassMate(name, age, height, techProblems , gender){
+	var student = {};
+	 
+		student.name = name;
+		student.age = age;
+		student.height = height;
+		student.techProblems = techProblems
+		student.gender = gender
+
+	return student
+}
+
+var taha= creatClassMate("taha",20,1.85,"some recusrion","male");
+var hamza= creatClassMate("hamza",19,1.83,"data modeling","male");
+var amin= creatClassMate("amin",22,1.84,"object","male");
+var adnen= creatClassMate("adnen",27,1.80,"high order functions","male");
+
+
+var arrayOfStudents = [taha,amin,hamza,adnen];
+
+function displayFriend(student){
+	return student.name +" is "+student.age +" years old and he have probelms with " +student.techProblems; 
+}
+
+function addFriend(student){
+	arrayOfStudents.push(student);
+}
+
+function isMale(student){
+	var s = 0;
+	for(var i = 0 ; i < student.length ; i++ ){
+		if(student[i].gender==="male"){
+			s+=1
+		}
+	}
+	return s
+}
+
+function search(array,name){
+	var str="INVALID NAME";
+	for(var i = 0 ; i < array.length ; i++ ){
+		if(array[i] === name){
+			str = displayFriend(name);				
+		}
+	}
+	return str
+}
