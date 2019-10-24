f19df645ebaacd00d6b9c19123adbee7af43d1b1
blusk1n <tahaznazen@gmail.com>
2019-10-24 09:35:54 +0100

    warmUp 7 solved

--- warmUp7.js
+++ warmUp7.js
@@ -4,3 +4,21 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+//1
+
+"Hamza" + "Danden"
+//2
+
+13 % 3 === 0;
+
+//3
+var sum = 0 ;
+var arr = [13,14,13,15,16,17,19,13,16,15];
+for(var i = 0 ; i < arr.length ; i++){
+	sum += arr[i];
+}
+var average = sum / arr.length
+
+//4
+var age = 20 ;
+var ageOnSecond = age * 365 * 24 * 60 + 60 
