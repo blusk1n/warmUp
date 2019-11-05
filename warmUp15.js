6432b20a7bce8874d21f08355ec0de361c0da8b3
blusk1n <tahaznazen@gmail.com>
2019-11-05 09:51:53 +0100

    warmUp15

--- warmUp15.js
+++ warmUp15.js
@@ -4,3 +4,28 @@
 //
 //
 // Note : you can`t use any built-in functions .
+function reversed(str){
+	var arr=[];
+	var lastArr=[];
+	var newStr="";
+	var tab = []
+	var i = 0 ;
+	while(i < str.length){
+		arr[i]=str[i]
+		i++
+	}
+	console.log(arr);
+	var j = 0 ;
+	var k = 0 ;
+	for (var j = 0; j < arr.length; j++) {
+		if(arr[j] === " " ||arr[j] === undefined ){
+			lastArr[k]=newStr
+			newStr = "";
+			k++
+		}
+		newStr += arr[j];
+		console.log(newStr)
+	}
+	
+
+}
