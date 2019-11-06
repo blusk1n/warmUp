f57a53837979b6fa5b80fdfaaed6531fbc479cc4
blusk1n <tahaznazen@gmail.com>
2019-11-06 09:50:25 +0100

    wam 16

--- warmUp16.js
+++ warmUp16.js
@@ -16,3 +16,35 @@
 // Constraints:
 // 1 <= text.length <= 10^4
 // text consists of lower case English letters only.
+// function formStr(str){
+// 	var arr = str.split("");
+// 	var finalArr = [];
+// 	for(var i = 0 ; i < arr.length ; i++){
+// 	var string = "";		
+// 	string += arr[Math.floor(Math.random() * Math.floor(arr.length-1))]
+// 	finalArr.push(string)
+// 	}
+	
+// 	return finalArr.join("")
+// }
+function makeStr(str){
+	var count = 0;
+	var arr1 = "balloon".split("");
+	var arr2 = str.split("");
+	var lastArr = [];
+	for(var i=0 ; i < arr2.length ; i++){
+		for(var j = 0 ; j < arr1.length ; j++){
+			if(arr2[i] === arr2[j]){
+				lastArr.push(arr2[i])
+			}
+		}
+	}
+
+	for(var i = 0 ; i < lastArr.length ; i++){
+		if(lastArr[i]===lastArr[i+1]){	
+			lastArr.splice(i,1)
+		}
+	}
+	console.log(lastArr)
+	
+}
